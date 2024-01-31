74f1fa3497bf9a5c8a0e3e0a64556cabb931e5f5
konditi1 <konditi1@gmail.com>
2024-01-31 19:32:50 +0300

    0. Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""" BaseCaching module
+"""
+
+
+class BaseCaching:
+    """ BaseCaching defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+    """
+    MAX_ITEMS = 4
+
+    def __init__(self):
+        """ Initialize
+        """
+        self.cache_data = {}
+
+    def print_cache(self):
+        """ Print the cache
+        """
+        print("Current cache:")
+        for key in sorted(self.cache_data.keys()):
+            print("{}: {}".format(key, self.cache_data.get(key)))
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        raise NotImplementedError("put must be implemented in your cache class")
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        raise NotImplementedError("get must be implemented in your cache class")
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+    """
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first_key = next(iter(self.cache_data))
+                del self.cache_data[first_key]
+                print("DISCARD: {}".format(first_key))
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
