4fae13e82e1cb9d117b0c9b03fd2945e4132476d
konditi1 <konditi1@gmail.com>
2024-01-31 19:52:03 +0300

    0. Basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,37 +1,5 @@
 #!/usr/bin/env python3
-""" BaseCaching module
-"""
-
-
-class BaseCaching:
-    """ BaseCaching defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
-    """
-    MAX_ITEMS = 4
-
-    def __init__(self):
-        """ Initialize
-        """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """ Print the cache
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
-
-    def put(self, key, item):
-        """ Add an item in the cache
-        """
-        raise NotImplementedError("put must be implemented in your cache class")
-
-    def get(self, key):
-        """ Get an item by key
-        """
-        raise NotImplementedError("get must be implemented in your cache class")
-
+from base_caching import BaseCaching
 
 """
 BasicCache which inherits from BaseCaching
@@ -49,10 +17,6 @@ class BasicCache(BaseCaching):
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                first_key = next(iter(self.cache_data))
-                del self.cache_data[first_key]
-                print("DISCARD: {}".format(first_key))
 
     def get(self, key):
         """ Get an item by key
