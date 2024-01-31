860aafaee1c1bf701ee3223d035599dd2ab14c23
konditi1 <konditi1@gmail.com>
2024-01-31 22:55:11 +0300

    MRU Caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+"""
+ MRU Caching module which inherits from
+ BaseCaching and is a caching system
+"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    MRUCache defines:
+    - constants of your caching system
+    - where your data are stored (in a dictionary)
+    """
+
+    def __init__(self):
+        """ Initialize
+        """
+        super().__init__()
+        self.cache_data = {}
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                if key in self.cache_data:
+                    self.cache_data.pop(key)
+                else:
+                    print("DISCARD: {}".format(list(self.cache_data.keys())[0])
+                          )
+                    self.cache_data.pop(list(self.cache_data.keys())[0])
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is not None and key in self.cache_data:
+            return self.cache_data.get(key, None)
